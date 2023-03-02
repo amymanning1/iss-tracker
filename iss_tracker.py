9d6b1323fd86f0308c5788cf6790867385d8231d
Amy Manning <amycurly.manning@gmail.com>
2023-03-02 17:29:23 +0000

    Simplified approach to delete/post queries

--- iss_tracker.py
+++ iss_tracker.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
-import xmltodict, requests, math, json
+import xmltodict, requests, math
 app = Flask(__name__)
 
 url='https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml'
 response = requests.get(url)
 data = xmltodict.parse(response.text)
-data_usable = json.dumps(data)
-data_usable = json.loads(data_usable)
 @app.route('/', methods=['GET'])
 def entire_set() -> dict:
     """
@@ -14,7 +12,7 @@ def entire_set() -> dict:
     Args: There are no arguments.
     Return: data (dict): A dictionary of dictionaries and lists containing the entire data set. 
     """
-    return data_usable
+    return data
 
 @app.route('/epochs', methods=['GET'])
 def get_epochs() -> list:
@@ -23,7 +21,7 @@ def get_epochs() -> list:
     Args: there are no parameters called in the function definition, however the dictionary 'data' is accessed in this function.
     Return: epoch_list (list): A list of all epochs in the dataset. 
     """
-    if data_usable == None:
+    if data == None:
         return []
         exit()
     offset = request.args.get('offset',0)
@@ -41,7 +39,7 @@ def get_epochs() -> list:
     epoch_list = []
     count = 0
     offset_count = 0
-    for d in data_usable['ndm']['oem']['body']['segment']['data']['stateVector']:
+    for d in data['ndm']['oem']['body']['segment']['data']['stateVector']:
         if count == limit:
            break
         offset_count = offset_count + 1
@@ -57,15 +55,15 @@ def state_vec(epoch) -> list:
     Args: epoch (str): An epoch referenced by the user in the query line that is available in the data set. 
     Return: spec_state (list): Information specific to a certain state vector queried by the user.
     """
-    if data_usable == None:
+    if data == None:
         return []
         exit()
     epoch_list = []
-    for d in data_usable['ndm']['oem']['body']['segment']['data']['stateVector']:
+    for d in data['ndm']['oem']['body']['segment']['data']['stateVector']:
         epoch_list.append(d['EPOCH'])
     if epoch in epoch_list:
         ind = epoch_list.index(epoch)
-        spec_state = data_usable['ndm']['oem']['body']['segment']['data']['stateVector'][ind] 
+        spec_state = data['ndm']['oem']['body']['segment']['data']['stateVector'][ind] 
         return spec_state
     else:
         return 'Error, please enter a valid Epoch value'
@@ -77,15 +75,15 @@ def calc_speed(epoch) -> float:
     Args: epoch (str): An epoch referenced by the user in the query line that is available in the data set.
     Return: speed (float): The instantaneous speed for a specific epoch calculated using the formula for speed from Cartesian velocity vectors. 
     """
-    if data_usable == None:
+    if data == None:
         return []
         exit()
     epoch_list = []
-    for d in data_usable['ndm']['oem']['body']['segment']['data']['stateVector']:
+    for d in data['ndm']['oem']['body']['segment']['data']['stateVector']:
         epoch_list.append(d['EPOCH'])
     if epoch in epoch_list:
         ind = epoch_list.index(epoch)
-        spec_state = data_usable['ndm']['oem']['body']['segment']['data']['stateVector'][ind]
+        spec_state = data['ndm']['oem']['body']['segment']['data']['stateVector'][ind]
         x_dot = float(spec_state['X_DOT']['#text'])
         y_dot = float(spec_state['Y_DOT']['#text'])
         z_dot = float(spec_state['Z_DOT']['#text'])
@@ -121,11 +119,8 @@ def delete_data() -> dict:
     """
     # need to save data as a json locally in order to delete
     # going to need to go back through and recode paths so I can have deletion privileges
-    global data_usable
-    print(type(data_usable))
-    data_usable = data_usable.clear()
-    with open('data_usable.json','w') as m:
-        json.dump(data_usable, m)
+    global data
+    data = data.clear()
     return []
 
 @app.route('/post-data', methods=['POST'])
@@ -133,11 +128,11 @@ def replace_data() -> dict:
     """
     This function replaces the data in data_usable using a 'get' request.
     Args: none
-    Returns: usable_data (dict): the data replaced. 
+    Returns: data (dict): the data replaced. 
     """
-    data_usable = json.dumps(data)
-    data_usable = json.loads(data_usable)
-    return data_usable
+    response = requests.get(url)
+    data = xmltodict.parse(response.text)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
